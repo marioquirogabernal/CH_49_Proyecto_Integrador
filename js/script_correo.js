dcf65af2354b514dd7d8aa642451b6a76760b93b
3elizalde09 <je.elizalde09@gmail.com>
2024-12-16 19:41:02 -0600

    Se modifica archivo script_correo.js, para agregar la funcion de validar nombre

--- js/script_correo.js
+++ js/script_correo.js
@@ -2,15 +2,29 @@ const txtNombre = document.getElementById("txtNombre")
 const txtEmail = document.getElementById("txtEmail")
 const txtTelefono = document.getElementById("txtTelefono")
 const txtMensaje = document.getElementById("txtMensaje")
-const btnEnviarCorreo = document.getElementById("btnEnviarCorreo")
+const btnEnviarCorreo = document.getElementById("btnEnviarCorreo");
 const formulario = document.getElementById("miFormulario");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 
-function validarNombre(){
-    //Aqui poner las validaciones de Nombre
-    return true
-}
+console.log("Prueba funcion validar name");
+
+
+//Crear variable para hacer referencia a id de las alertas para nombre.
+const alertName = document.getElementById("alertName");
+
+//Crear variable para hacer referencia a id de las alertas para nombre. Crear texto de error.
+const alertNameTxt = document.getElementById("alertNameTxt");
+
+
+
+
+//Creacion de funcion validar nombre.
+function validarName(){
+    const name = txtNombre.value.trim();
+    reGex = /^[A-Za-z_-]{3,15}$/;
+    return reGex.test(name);
+}//funcionValidarName
 
 function validarEmail() {
     const email = txtEmail.value.trim();
@@ -35,6 +49,22 @@ btnEnviarCorreo.addEventListener("click", (event) =>{
     txtEmail.value = txtEmail.value.trim();;
     alertValidacionesTexto.innerHTML = "";
     alertValidaciones.style.display = "none";
+
+    txtNombre.style.border = "";
+    txtNombre.value = txtNombre.value.trim();
+
+    // let isvalid = true;
+
+    
+    alertNameTxt.innerHTML="";
+    alertName.style.display = "none";
+
+    if(!validarName()){
+        txtNombre.style.border = "solid red medium";
+        alertNameTxt.innerHTML += "<br/> <strong> El nombre es invalido</strong>";
+        alertName.style.display = "block";
+        isvalid = false
+    }
 
     if (!validarEmail()) { // si regresa false
         txtEmail.style.border = "solid red medium";
