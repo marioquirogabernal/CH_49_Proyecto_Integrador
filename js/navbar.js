8795a936c577e42d378aa872052d21e3e3e1ed7c
ErickDominguez <erick_dominguez_santiago@outlook.es>
2024-12-19 16:02:10 -0600

    se añade el header dinamico

---
+++ js/navbar.js
@@ -0,0 +1,56 @@
+const bodyTag = document.getElementsByTagName("body").item(0);
+const headTag = document.getElementsByTagName("head").item(0);
+
+// <link rel="stylesheet" href="./css/header.css" />
+window.addEventListener("load", function(e){
+    headTag.insertAdjacentHTML("beforeend",
+        `<link rel="stylesheet" href="./css/header.css" />`
+    );
+    
+    bodyTag.insertAdjacentHTML("afterbegin", 
+        `<div class="container-header">
+        <nav class="navbar navbar-expand-lg bg-body-tertiar">
+            <div class="container-fluid container-header">
+            <div class="col-sm">
+                <a class="navbar-brand nav-link" href="index.html">
+                <img src="./img/logo.png" alt="Logo" width="57" height="57"
+                    class="d-inline-block align-text-center" />
+                TECNOLOGY-SHOP
+                </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <div class="col-sm">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.html">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="productos.html">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="contacto.html">Contacto
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="Nosotros.html">Nosotros</a>
+                    </li>
+                </ul>
+                </div>
+                <ul class="navbar-nav d-flex">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Iniciar sesión</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Registrarse</a>
+                </li>
+                <a class="nav-link" aria-current="page" href="#">Carrito</a>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        </div>`)
+});
