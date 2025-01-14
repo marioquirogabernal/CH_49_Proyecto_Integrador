ba2abf3e14d7b03e11be0cf73937e6e98e874d4b
Adrian Martinez Calderon <adrianmtzcl@gmail.com>
2025-01-14 13:38:13 -0600

    fix: se corrigieron algunos bugs del footer y de la pagina de contacto

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var footerEnd = `
+    headTag.insertAdjacentHTML("beforeend",
+        `<link rel="stylesheet" href="./css/footer.css" />`      
+    );
+    
+
+    bodyTag.insertAdjacentHTML("beforeend", 
+        `
         <footer id="footer" link rel="stylesheet" href="./css/footer.css">
-                <a href="#">¿Quiénes somos?  |  </a>
+                <a href="../Nosotros.html">¿Quiénes somos?  |  </a>
                 <a href="#">Aviso de privacidad  |  </a>
                 <a href="#">Preguntas frecuentes</a>
         </footer>
-    `;
-    document.body.insertAdjacentHTML("beforeend", footerEnd);
+    `)
+
+
+
 });
